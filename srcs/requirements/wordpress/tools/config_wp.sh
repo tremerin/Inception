f26da346b36728b0310a2166d59505e484cf02d6
fgalan-r <tremerin13@gmail.com>
2024-03-20 01:30:05 +0100

    wp config-wp coment

--- srcs/requirements/wordpress/tools/config_wp.sh
+++ srcs/requirements/wordpress/tools/config_wp.sh
@@ -3,11 +3,14 @@
 cd /var/www/html
 
 if [ ! -f ./wp-config.php ]; then
+	# descarga wordpress
 	wp core download --allow-root
+	# configuar el archivo de wp con los parametros de la base de datos
 	wp config create --allow-root --dbname=$SQL_DB --dbuser=$SQL_USER --dbpass=$SQL_PWD --dbhost=mariadb --path='/var/www/html'
+	# instala wp
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	# crea el usuario de wp
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-	#wp theme activate twentytwentytwo --allow-root
 fi
 
 mkdir /run/php
